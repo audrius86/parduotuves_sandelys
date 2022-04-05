6e8014e7052d2f99ea14f0d3768e7ac3e09ff411
Audrius <audrius.bareisis@gmail.com>
2022-04-05 21:13:33 +0300

    Užduoties aprašymas

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
-echo date('Y-m-d');
-echo "labas rytas";
-echo "kazka darom";
-echo "bandom";
+/**
+ * prisijungimas
+ * registracija su rolemis (sandelio darbuotojas, parduotuves darbuotojas)
+ *
+ * //sandelio darbuotojas
+ * prekiu kategorijos (duona, pieno produktai, kaceliarija ... )
+ * prekiu valdymas (prideti, atnaujinti, istrinti) - prekės yra priskirtos prie parduotuvės
+ * sandėlis - jame saugomos visos prekiu sarasas ir yra maksimalus vienetu skaicius
+ *
+ * parduotuviu uzsakymai is sandelio (sandelio istorija)
+ *
+ * //parduotuves darbuotojas
+ * parduotuviu valdymas (prideti, atnaujinti, istrinti) (bendra marzas, papildomos marzos pagal kategorijas arba akcijos)
+ * parduotuves prekiu informacija (galiojimo laikas, kiekis/vienetai, kaina
+ * prekes kurias reikejo ismesti (kuriu galiojimas baigiasi) parduotuve
+ *
+ * pirkejas ir jo krepselis(kur ir ka pirko ir kiek sumokejo)
+ *
+ * statistika (kiek parduotuves uzdirbo, kokios prekes populiariausios)
+ */
+
 ?>
