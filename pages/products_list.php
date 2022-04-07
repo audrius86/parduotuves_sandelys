5ee870875fbd8137f7022f2d068067987099885f
Audrius <audrius.bareisis@gmail.com>
2022-04-07 16:13:41 +0300

    Created "Add new product with quantity or update existing product quantity to warehouse products list functionality"

--- pages/products_list.php
+++ pages/products_list.php
@@ -1,8 +1,25 @@
+<?php
+if(isset($_POST['quantity'])){
+    $product_id = $_POST['product_id'];
+    $quantity = $_POST['quantity'];
+
+    $sql = "SELECT product_id, quantity FROM warehouse_products WHERE product_id='$product_id'";
+    $result = mysqli_fetch_assoc(mysqli_query($connection, $sql));
+    $updated_quantity = $result['quantity'] + $quantity;
+    if ($result) {
+        $sql = "UPDATE warehouse_products SET quantity='$updated_quantity' WHERE product_id='$product_id'";
+        $result = mysqli_query($connection, $sql);
+        }else{
+        $sql = "INSERT INTO warehouse_products (product_id, quantity) VALUES ('$product_id','$quantity')";
+    }
+}
+?>
+
 <h1>Products List</h1>
 
 <table class="products_list">
     <tr>
-        <th>Id</th>
+        <th>No</th>
         <th>Category</th>
         <th>Product</th>
         <th>Price</th>
@@ -14,8 +31,9 @@
     $result = mysqli_query($connection, $sql);
     while ($row = mysqli_fetch_array($result)) { ?>
     <tr>
-        <form>
+        <form action="#" method="post">
         <td>
+            <input type="hidden" id="product_id" name="product_id" value="<?php echo $row['id'] ?>">
             <?php echo $row['id'] ?>
         </td>
         <td>
@@ -28,7 +46,7 @@
             <?php echo $row['price'] ?>
         </td>
         <td>
-            <input type="number" placeholder="Quantity">
+            <input type="number" name="quantity" min="1" placeholder="Quantity">
         </td>
         <td>
             <input type="submit" value="Add To Warehouse">
